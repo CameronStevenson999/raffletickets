90c3687669c2969b1a93a288639b31030646349b
Cameron Stevenson <cstev999@gmail.com>
2025-04-01 18:28:19 +1000

    Add Swagger and model and context

--- booktickets/Program.cs
+++ booktickets/Program.cs
@@ -1,9 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi(); // UseSwaggerUI Protected by if (env.IsDevelopment())
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
